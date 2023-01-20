202fce999565e91464f7c0d260db3b50a274acba
sujin-kk <sjoo0002@gmail.com>
2023-01-20 15:54:40 +0900

    [BasicState]7-컴포지션의 Remember

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/component/WaterCount.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/component/WaterCount.kt
@@ -1,12 +1,14 @@
 package com.codelabs.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.codelabs.basicstatecodelab.component.WellnessTaskItem
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
@@ -14,15 +16,29 @@ fun WaterCounter(modifier: Modifier = Modifier) {
         var count by remember { mutableStateOf(0) }
 
         if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you taken your 15 minute walk today?",
+                    onClose = { showTask = false }
+                )
+            }
             Text("You've had $count glasses")
         }
 
-        Button(
-            onClick = { count++ },
-            enabled = count < 10,
-            modifier = Modifier.padding(top = 8.dp)
-        ) {
-            Text("Add One")
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(
+                onClick = { count++ },
+                enabled = count < 10,
+                modifier = Modifier.padding(end = 8.dp)
+            ) {
+                Text("Add One")
+            }
+            Button(
+                onClick = { count = 0 },
+            ) {
+                Text("Clear water count")
+            }
         }
     }
 }
