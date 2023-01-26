deb5a08b0398f3cbe48d85051c783ede19f87c2b
sujin-kk <sjoo0002@gmail.com>
2023-01-26 18:23:29 +0900

    [BasicState]11-관찰 가능한 MutableList

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/component/WellnessScreen.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/component/WellnessScreen.kt
@@ -2,13 +2,18 @@ package com.codelabs.basicstatecodelab.component
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+import com.codelabs.basicstatecodelab.model.WellnessTask
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
     // WaterCounter(modifier)
-    Column(modifier) {
+    Column(modifier = modifier) {
         StatefulCounter()
-        WellnessTasksList()
+
+        val list = remember { WellnessTask.getWellnessTasks().toMutableStateList() }
+        WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task) })
     }
 }
--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/component/WellnessTasksList.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/component/WellnessTasksList.kt
@@ -3,18 +3,21 @@ package com.codelabs.basicstatecodelab.component
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.codelabs.basicstatecodelab.model.WellnessTask
 
 @Composable
 fun WellnessTasksList(
     modifier: Modifier = Modifier,
-    list: List<WellnessTask> = remember { WellnessTask.getWellnessTasks() },
+    onCloseTask: (WellnessTask) -> Unit,
+    list: List<WellnessTask>
 ) {
     LazyColumn(modifier = modifier) {
-        items(list) { task ->
-            WellnessTaskItem(taskName = task.label)
+        items(
+            items = list,
+            key = { task -> task.id }
+        ) { task ->
+            WellnessTaskItem(taskName = task.label, onClose = { onCloseTask(task) })
         }
     }
 }
