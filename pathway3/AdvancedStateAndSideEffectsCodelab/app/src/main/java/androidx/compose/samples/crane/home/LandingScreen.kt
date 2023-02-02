eef994ed667b06c7bb504fe0b85d4c98411ff253
sujin-kk <sjoo0002@gmail.com>
2023-02-02 14:42:18 +0900

    [AdvancedAndSideEffect]4-LaunchedEffect 및 rememberUpdatedState

--- pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
+++ pathway3/AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
@@ -20,18 +20,34 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.samples.crane.R
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import kotlinx.coroutines.delay
 
 private const val SplashWaitTime: Long = 2000
 
 @Composable
 fun LandingScreen(modifier: Modifier = Modifier, onTimeout: () -> Unit) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        // TODO Codelab: LaunchedEffect and rememberUpdatedState step
-        // TODO: Make LandingScreen disappear after loading data
+
+        // LaunchedEffect가 컴포지션을 시작하면 코루틴 실행
+        // LaunchedEffect가 컴포지션을 종료하면 코루틴 취소
+
+        // key1 key2 ... -> key중 하나가 변경될때마다 효과 다시 시작
+        // onTimeout이 변경되더라도 LandingScreen이 나타나서는 안된다!
+
+        // onTimeout이 변경될 때 값을 업데이트 -> 가장 마지막 값만 변경할 수 있는 rememberUpdatedState
+        val currentOnTimeout by rememberUpdatedState(onTimeout)
+
+        LaunchedEffect(onTimeout) {
+            delay(SplashWaitTime)
+            currentOnTimeout()
+        }
         Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
     }
 }
