499fa5a30204f5171eeb49991cbb2c8e06ed442c
sujin-kk <sjoo0002@gmail.com>
2023-02-16 15:35:29 +0900

    [Testing]5-merged & unmerged semantic tree

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -2,7 +2,6 @@ package com.example.compose.rally
 
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.text.toUpperCase
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import org.junit.Before
 import org.junit.Rule
@@ -36,7 +35,7 @@ class TopAppBarTest {
             .assertIsSelected()
     }
 
-    // Semantic Tree -> 화면에서 요소를 찾고 해당 속성을 읽는 구조
+    // Semantics Tree -> 화면에서 요소를 찾고 해당 속성을 읽는 구조
     // 탭 선택했을 때 text가 잘 노출되는지
     @Test
     fun rallyTopAppBarTest_currentLabelExists() {
@@ -48,10 +47,22 @@ class TopAppBarTest {
             )
         }
 
-        composeTestRule.onRoot().printToLog("currentLabelExists")
+        // UnMergedTree ??
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("currentLabelExists")
 
+        // text -> ACCOUNT 인 노드
+        // ContentDescription -> Accounts 인 노드를 부모로 가지는 노드
         composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
-            .assertExists()
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                hasParent(
+                    hasContentDescription(RallyScreen.Accounts.name)
+                ),
+                useUnmergedTree = true
+            )
+
+//        composeTestRule
+//            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+//            .assertExists()
     }
 }
