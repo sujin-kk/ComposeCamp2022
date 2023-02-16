6cc60f77b1aa8c223569f40fe1e2146a07cfc58e
sujin-kk <sjoo0002@gmail.com>
2023-02-16 15:16:27 +0900

    [Testing] 4-디버깅 테스트

--- pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ pathway4/TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,10 +1,10 @@
 package com.example.compose.rally
 
-import androidx.compose.ui.test.assertIsSelectable
-import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.text.toUpperCase
 import com.example.compose.rally.ui.components.RallyTopAppBar
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -13,10 +13,15 @@ class TopAppBarTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
+    lateinit var allScreens: List<RallyScreen>
+
+    @Before
+    fun setUp() {
+        allScreens = RallyScreen.values().toList()
+    }
+
     @Test
     fun rallyTopAppBarTest() {
-        val allScreens = RallyScreen.values().toList()
-
         composeTestRule.setContent {
             RallyTopAppBar(
                 allScreens = allScreens,
@@ -25,9 +30,28 @@ class TopAppBarTest {
             )
         }
 
-        // AppBar에서 선택이 잘 되는지 Test
+        // 현재 Accounts 화면이 select 됐는지
         composeTestRule
             .onNodeWithContentDescription(RallyScreen.Accounts.name)
             .assertIsSelected()
+    }
+
+    // Semantic Tree -> 화면에서 요소를 찾고 해당 속성을 읽는 구조
+    // 탭 선택했을 때 text가 잘 노출되는지
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule.onRoot().printToLog("currentLabelExists")
+
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertExists()
     }
 }
